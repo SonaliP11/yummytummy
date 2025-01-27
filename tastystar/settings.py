3a0292eb992085af5c97151c6ed0b285d27a99f6
SonaliP11 <sonali.jain433@gmail.com>
2025-01-27 13:47:55 +0000

    readies code for deploy

--- tastystar/settings.py
+++ tastystar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a3ebh-5pc+e2cp_utiu*4o!4$#1jg#%v_nj9h_$n4odyx1sl=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
