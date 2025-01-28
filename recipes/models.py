fae0c714d534e853f17b7fd27caf3f73e154102b
SonaliP11 <sonali.jain433@gmail.com>
2025-01-28 14:47:37 +0000

    Add Comment model and update Recipe model fields; register models in admin

--- recipes/models.py
+++ recipes/models.py
@@ -10,21 +10,46 @@ MEAL_TYPES = (
     ('dinner', 'Dinner'),
 )
 
+STAR_RATINGS = (
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    )
+
 # Create your models here.
 class Recipe(models.Model):
     """
     Model representing a recipe
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_owner') 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_owner') 
     title = models.CharField(max_length=200, unique=True)
     description = models.TextField()
     ingredients = models.TextField()
     steps = models.TextField()
-    posted_date = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default='breakfast')
     class Meta:
-        ordering = ["-posted_date"]  
+        ordering = ["-created_on"]  
     
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    """
+    Model representing a comment
+    """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    rating = models.IntegerField(choices=STAR_RATINGS, default=1)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]  
+    
+    def __str__(self):
+        f"Comment {self.body} by {self.author}"
