dd1f83011274a5682723061c114e19e126e4e2b9
SonaliP11 <sonali.jain433@gmail.com>
2025-01-29 10:58:44 +0000

    Implement Recipe list view and update URL routing

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Recipe
 
-def my_recipes(request):
-    return HttpResponse("Hello, Recipe!")
 
 # Create your views here.
+class RecipeList(generic.ListView):
+    model = Recipe
