15076c39059c9c3ccf03e93dba610cbb4094784e
SonaliP11 <sonali.jain433@gmail.com>
2025-01-29 22:55:21 +0000

    Integrate django-summernote for enhanced recipe editing in admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Recipe) 
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'meal_type', 'author','status')
+    search_fields = ['title']
+    list_filter = ('status', 'meal_type',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description', 'ingredients', 'steps',)
 
 # Register your models here.
-admin.site.register(Recipe)
 admin.site.register(Comment)
