c8de2d64ffe788dfef66222215377f06ca572b93
SonaliP11 <sonali.jain433@gmail.com>
2025-01-30 09:46:38 +0000

    Replace recipe_list.html with index.html for improved layout and pagination

--- recipes/views.py
+++ recipes/views.py
@@ -6,11 +6,9 @@ from .models import Recipe
 # Create your views here.
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
-    template_name = 'recipes/recipe_list.html'
+    template_name = 'recipes/index.html'
+    paginate_by = 6
 
-class RecipeDetail(generic.DetailView):
-    model = Recipe
-    template_name = 'recipes/recipe_detail.html'
     
 # Compare this snippet from recipes/views.
 
