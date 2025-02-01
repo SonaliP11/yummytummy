cdbc3073fe7aa5c4f2dcb9eff8b23562c126fc8f
SonaliP11 <sonali.jain433@gmail.com>
2025-02-01 00:28:58 +0000

    Add recipe detail view and update index layout

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -7,7 +7,32 @@ from .models import Recipe
 class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'recipes/index.html'
-    paginate_by = 6
+    paginate_by = 3
+
+
+def recipe_detail(request, slug):
+    """
+    Display an individual :model:`recipes.Recipe`.
+
+    **Context**
+
+    ``recipe``
+        An instance of :model:`recipes.Recipe`.
+
+    **Template:**
+
+    :template:`recipes/recipe_detail.html`
+    """
+    template_name = 'recipes/recipe_detail.html'
+    
+    queryset = Recipe.objects.filter(status=1)
+    recipe = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request, 
+        template_name, 
+        {"recipe": recipe},
+        )
 
     
 # Compare this snippet from recipes/views.
