73122386b8fa1a236099c1afb9a66259867fb034
SonaliP11 <sonali.jain433@gmail.com>
2025-02-03 15:05:32 +0000

    Add filtering by meal type and comment functionality to recipe views

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
+from .forms import CommentForm
 
 
 # Create your views here.
 class RecipeList(generic.ListView):
-    queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+    model = Recipe  
     template_name = 'recipes/index.html'
     paginate_by = 9
+    
+    def get_queryset(self):
+        queryset = Recipe.objects.filter(status=1).order_by('-created_on')
+        meal_type = self.kwargs.get('meal_type', None)
+        if meal_type:
+            queryset = queryset.filter(meal_type=meal_type)
+        return queryset
 
 
 def recipe_detail(request, slug):
@@ -30,6 +38,17 @@ def recipe_detail(request, slug):
     comments = recipe.comments.all().order_by("-created_on")
     comment_count = recipe.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.recipe = recipe
+            comment.save()
+
+
+    comment_form = CommentForm()
+
 
     return render(
         request, 
@@ -37,7 +56,8 @@ def recipe_detail(request, slug):
         {
             "recipe": recipe,
             "comments": comments,
-            "comment_count": comment_count,     
+            "comment_count": comment_count,
+            "comment_form": comment_form,    
          },
         )
 
